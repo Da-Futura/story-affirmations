7f69aa8ce9d1957f7d65176ca8c1506e7e1a943d
Jean-Le Des Etages <ronald@portalengineering.net>
2019-04-23 10:46:46 -0400

    Add initial Player and Story classes

--- src/api/story_generator.py
+++ src/api/story_generator.py
@@ -1,5 +1,27 @@
 print("Hello World")
 
+class Player(object):
+    """The current player"""
+    def __init__(self, name, personal, posessive):
+        super(Player, self).__init__()
+        self.name = name
+        self.personal_pronoun = personal        
+        self.posessive_pronoun = posessive
+        
+
+class Story(object):
+    """Initial Story Object"""
+    def __init__(self, player):
+        super(Story, self).__init__()
+        self.player = player
+    
+    def create_story(self):
+        player = self.player
+        return player.posessive_pronoun
+                
+
 def create_story(name):
     """Creates a story given the user's name"""
-    return name
+    player = Player('Karo', 'she', 'her')
+    my_story = Story(player)
+    return my_story.create_story()
